e690aaa4d2cb43daad3e761ad4878d3bd3b1df94
Berat Özlü <112069768+beratozlu@users.noreply.github.com>
2025-06-21 19:59:26 +0300

    Update KullanıcıVerileri.sql

--- KullanıcıVerileri.sql
+++ KullanıcıVerileri.sql
@@ -1,5 +1,5 @@
 	INSERT INTO Kullanicilar (AdSoyad, DogumTarihi, Cinsiyet, Boy, Kilo)
 VALUES
-('Berat �zl�', '2001-10-31', 'Erkek', 175, 76.5),
-('Ahmey Utku �olak', '2004-08-05', 'Erkek', 190, 105),
-('Steven Tyler', '1948-03-26', 'Erkek', 177, 80);
+('Berat OZLU', '2001-10-31', 'Erkek', 175, 76.5),
+('Ahmet Utku COLAK', '2004-08-05', 'Erkek', 190, 105),
+('Steven TYLER', '1948-03-26', 'Erkek', 177, 80);
